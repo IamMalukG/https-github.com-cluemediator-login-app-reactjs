28c12978689b7cca9bf349ca72df0473eb0925ef
IamMalukG <malik.habib.ciit.065@gmail.com>
2020-12-17 13:44:37 +0500

    Initial commit

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      Welcome to the Home Page!
+      <center>
+        
+        <br /> <b> CONGRATS </b>
+      </center>
+    </div>
+  );
+}
+
+export default Home;
---
+++ README.md
@@ -0,0 +1,3 @@
+# login-app-reactjs-k86cem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/login-app-reactjs-k86cem)
